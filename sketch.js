6f48cf12bbbf176d2e35d5e1f7bd48b6450e9bd3
Siddharth Santosh <79411827+siddharthsantosh816@users.noreply.github.com>
2021-05-05 21:26:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,11 +3,11 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 var plinko, ground1;
-var check = 0;
+var check = 0,R =0;
 var particles = [];
 var plinkos=[];
 var divisions=[];
-var score=0, attempts=5;
+var score=0, attempts=5, pCount=0;
 var PLAY=1;
 var END=0;
 var gameState=PLAY;
@@ -68,12 +68,11 @@ function draw() {
         textSize(18);
         text(d,x,480)
     }
-    
-     
+         
     if (gameState === END) {
         textSize(40);
         fill("red");
-        text("Game Over", 300, 340);
+        text("Game Over", 300, 250);
         textSize(18);
         text("Press 'R' to restart the game", 300, 30)
     } 
@@ -82,24 +81,23 @@ function draw() {
         plinkos[i].display();   
     } 
    
-
     for(var i=0; i <divisions.length; i++){
         divisions[i].display();  
     } 
-    if (particles.length>0){
+
     for(var i=0; i<particles.length; i++){ 
         particles[i].display();
         var num = particles.length-1;
         console.log(num);
     }
-}
+
     //scoring
-    if(check===0 && particles.length>0){
-        if(particles[num].body.position.y>470 &&particles[num].body.position.x > 5 && particles[num].body.position.x < 795 ){
+    if(check===0 && particles.length>0 && R===0){
+        if(particles[num].body.position.y>460 &&particles[num].body.position.x > 5 && particles[num].body.position.x < 795 ){
             if(particles[num].body.position.x<320){
                 score=score+100;
                 scoreSound.play();
-                check=1;
+                check=1; 
             }
             else if(particles[num].body.position.x<480){
                 score=score+500;
@@ -116,7 +114,6 @@ function draw() {
     if(attempts===0){
         gameState=END;
     }
-
  }
  
 function mousePressed() {
@@ -124,14 +121,17 @@ function mousePressed() {
         particles.push(new Particle(mouseX,10,8,6));
         attempts = attempts -1;
         check=0;
+        R = 0;
     } 
 }
+
 function keyPressed() {
     if (keyCode === 82 && gameState === END) {   
         score=0;
         attempts=5;
         check = 0;
-        particles = [];
+        R=1
+    //  particles = [];
         gameState = PLAY;
     }
 }
